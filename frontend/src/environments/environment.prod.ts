758e16bcb007479b7023d48fdc81a4f7277ebce3
Alex Trytetskyi <alextrytetskyi@MacBook-Pro-Alex.local>
2022-08-11 14:42:57 +0300

    Inint Project

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  url: 'https://api.openweathermap.org/data/2.5',
+  appId: '5a4b2d457ecbef9eb2a71e480b947604',
+  iconUrl: 'https://www.angulartraining.com/images',
+};
